68ea3e533f8604ffadd42621a7786f3d3446f27f
PogorelovaTeacher <pogorelova_marin@mail.ru>
2020-05-15 15:05:27 +0300

    Change color form

--- WindowsFormsAppTestGit/Form1.Designer.cs
+++ WindowsFormsAppTestGit/Form1.Designer.cs
@@ -64,6 +64,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Highlight;
             this.ClientSize = new System.Drawing.Size(517, 168);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button2);
